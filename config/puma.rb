bbf654325637a46c7f29350a9f68aaee17eddf75
Joe Basirico <joebasirico@users.noreply.github.com>
2025-11-14 14:56:50 -0800

    Update puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -27,15 +27,49 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
+# Bind to Unix socket for production, port for development
+if ENV["RAILS_ENV"] == "production"
+  # Get the app root directory
+  app_dir = File.expand_path("..", __dir__)
 
-# Allow puma to be restarted by `bin/rails restart` command.
+  bind "unix://#{app_dir}/tmp/sockets/puma.sock"
+  pidfile "#{app_dir}/tmp/pids/puma.pid"
+  state_path "#{app_dir}/tmp/pids/puma.state"
+  stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
+
+  # Workers for production (2 workers recommended for 4GB RAM)
+  workers ENV.fetch("WEB_CONCURRENCY", 2)
+
+  # Preload app for better memory usage with workers
+  preload_app!
+
+  # Restart workers periodically to prevent memory bloat
+  before_fork do
+    ActiveRecord::Base.connection_pool.disconnect! if defined?(ActiveRecord)
+  end
+
+  on_worker_boot do
+    ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+  end
+else
+  port ENV.fetch("PORT", 3000)
+end
+
 plugin :tmp_restart
-
-# Run the Solid Queue supervisor inside of Puma for single-server deployments
 plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
 
-# Specify the PID file. Defaults to tmp/pids/server.pid in development.
-# In other environments, only set the PID file if requested.
-pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
+# threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
+# threads threads_count, threads_count
+
+# # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+# port ENV.fetch("PORT", 3000)
+
+# # Allow puma to be restarted by `bin/rails restart` command.
+# plugin :tmp_restart
+
+# # Run the Solid Queue supervisor inside of Puma for single-server deployments
+# plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
+
+# # Specify the PID file. Defaults to tmp/pids/server.pid in development.
+# # In other environments, only set the PID file if requested.
+# pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
